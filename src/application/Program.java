e840db3595aae6ea1b7931e22f20106601379f6a
Jean Gabriel <jeangabriel180@hotmail.com>
2022-03-10 22:32:03 -0300

    criacao projeto

---
+++ src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+import services.PrintService;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PrintService<String> ps = new PrintService<>();
+
+        System.out.println("How many values? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String value = sc.next();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        System.out.println("First: " + ps.first());
+        sc.close();
+    }
+}
